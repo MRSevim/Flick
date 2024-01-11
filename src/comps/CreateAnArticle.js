c340f0e5e46a0d61046a4ae346b787862fee2f1f
MRSevim <lionrasit@gmail.com>
2024-01-11 21:52:59 +0300

    tinyMCE integration

--- src/comps/CreateAnArticle.js
+++ src/comps/CreateAnArticle.js
@@ -1,5 +1,11 @@
 import React from "react";
+import { Editor } from "@tinymce/tinymce-react";
 
 export const CreateAnArticle = () => {
-  return <div>CreateAnArticle</div>;
+  return (
+    <>
+      <div>TincyMCE kullan WYSIWYG editor</div>
+      <Editor></Editor>
+    </>
+  );
 };
--- src/comps/MostLiked.js
+++ src/comps/MostLiked.js
@@ -1,5 +1,20 @@
 import React from "react";
 
 export const MostLiked = () => {
-  return <div>MostLiked</div>;
+  return (
+    <div className="container my-4 ">
+      <section className="row g-3">
+        <div className="col col-12 col-md-4  text-center">
+          <div className="bg-warning">
+            <div className="border-bottom">Most Liked This Week</div>
+            <div className="border-bottom">Most Liked This Month</div>
+            <div className="border-bottom">Most Liked This Year</div>
+          </div>
+        </div>
+        <div className="col col-12 col-md-8">
+          <div className="border border-dark p-3">xdd</div>
+        </div>
+      </section>
+    </div>
+  );
 };
