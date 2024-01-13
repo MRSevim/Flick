b501b7244d692924f9cedb687d8f3e0f613f7577
MRSevim <lionrasit@gmail.com>
2024-01-13 14:02:36 +0300

    tinymce improvements

--- src/comps/CreateAnArticle.js
+++ src/comps/CreateAnArticle.js
@@ -1,11 +1,63 @@
+import { useEffect, useRef, useState } from "react";
+import DOMPurify from "dompurify";
 import React from "react";
 import { Editor } from "@tinymce/tinymce-react";
 
 export const CreateAnArticle = () => {
+  const editorRef = useRef(null);
+  const [content, setContent] = useState(null);
+  const [initialContent, setInitialContent] = useState(
+    "<p>Start by replacing this.</p>"
+  );
+
+  useEffect(() => {
+    const localStorageContent = JSON.parse(
+      localStorage.getItem("editor-content")
+    );
+    if (localStorageContent) {
+      setInitialContent(localStorageContent);
+    }
+  }, []);
+
+  const submit = () => {
+    if (editorRef.current) {
+      setContent(editorRef.current.getContent());
+    }
+  };
+
+  const saveDraft = () => {
+    if (editorRef.current) {
+      console.log(editorRef.current.getContent());
+    }
+  };
+
   return (
     <>
-      <div>TincyMCE kullan WYSIWYG editor</div>
-      <Editor></Editor>
+      <div className="create-an-article container mt-3">
+        <Editor
+          onInit={(evt, editor) => (editorRef.current = editor)}
+          initialValue={initialContent}
+          init={{ height: 600, menubar: false }}
+          onEditorChange={(newValue, editor) => {
+            let content = editor.getContent();
+            localStorage.setItem("editor-content", JSON.stringify(content));
+          }}
+        ></Editor>
+        <div className="mt-3 d-flex justify-content-center">
+          <button className="btn btn-lg me-3 btn-warning" onClick={saveDraft}>
+            Save Draft
+          </button>
+          <button className="btn btn-lg btn-warning" onClick={submit}>
+            Submit
+          </button>
+        </div>
+        {content && (
+          <div
+            className="mt-3 article "
+            dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(content) }}
+          ></div>
+        )}
+      </div>
     </>
   );
 };
