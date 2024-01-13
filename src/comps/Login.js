3497213148afadbab5905274ee6bed444e32a380
MRSevim <lionrasit@gmail.com>
2024-01-13 22:20:50 +0300

    implemented modal

--- src/comps/Login.js
+++ src/comps/Login.js
@@ -1,5 +1,99 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import * as bootstrap from "bootstrap";
 
 export const Login = () => {
-  return <div>Login</div>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      const modal = new bootstrap.Modal(document.getElementById("Modal"));
+      modal.show();
+    }
+  }, [isLoggedIn]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const userInfo = {
+      username,
+      password,
+    };
+    console.log(userInfo);
+    setIsLoggedIn(true);
+  };
+
+  return (
+    <>
+      <div className="container mt-5 d-flex justify-content-center">
+        <form className="" onSubmit={handleSubmit}>
+          <div className="form-group ">
+            <label>
+              Username:
+              <input
+                value={username}
+                onChange={(e) => {
+                  setUsername(e.target.value);
+                }}
+                className="form-control form-control-lg wide-input"
+                type="text"
+                required
+              />
+            </label>
+          </div>
+          <div className="form-group">
+            <label>
+              Password:
+              <input
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+                className="form-control form-control-lg wide-input"
+                type="password"
+                required
+              />
+            </label>
+          </div>
+          <input className="btn btn-warning mt-3" type="submit" value="Login" />
+        </form>
+      </div>
+      <div
+        className="modal fade"
+        id="Modal"
+        tabIndex="-1"
+        aria-labelledby="ModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="ModalLabel">
+                Action
+              </h5>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">This action does nothing for now.</div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-bs-dismiss="modal"
+                onClick={() => {
+                  setIsLoggedIn(false);
+                }}
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
--- src/comps/SignUp.js
+++ src/comps/SignUp.js
@@ -1,5 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const SignUp = () => {
-  return <div>SignUp</div>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const userInfo = {
+      username,
+      password,
+      email,
+    };
+    console.log(userInfo);
+  };
+  return (
+    <div className="container mt-5 d-flex justify-content-center">
+      <form className="" onSubmit={handleSubmit}>
+        <div className="form-group ">
+          <label>
+            Username:
+            <input
+              value={username}
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
+              className="form-control form-control-lg wide-input"
+              type="text"
+              required
+            />
+          </label>
+        </div>
+        <div className="form-group">
+          <label>
+            Password:
+            <input
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+              className="form-control form-control-lg wide-input"
+              type="password"
+              required
+            />
+          </label>
+        </div>
+        <div className="form-group">
+          <label>
+            E-mail:
+            <input
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+              className="form-control form-control-lg wide-input"
+              type="email"
+              required
+            />
+          </label>
+        </div>
+        <input className="btn btn-warning mt-3" type="submit" value="Sign-up" />
+      </form>
+    </div>
+  );
 };
