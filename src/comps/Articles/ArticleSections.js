b4ee329da8b8141739c993210735c6afce07c832
MRSevim <lionrasit@gmail.com>
2024-02-26 16:30:05 +0300

    implementing sections logic

--- src/comps/Articles/ArticleSections.js
+++ src/comps/Articles/ArticleSections.js
@@ -1,7 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./ArticleSections.css";
 
+function insertAfter(referenceNode, newNode) {
+  referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
+
 export const ArticleSections = ({ sections }) => {
+  const [loading, setLoading] = useState(true);
+
   const toggleHeadersBelow = (e) => {
     e.target.classList.toggle("open");
     const headers = document.querySelectorAll(".header");
@@ -9,124 +15,111 @@ export const ArticleSections = ({ sections }) => {
       headers,
       e.target.parentNode
     );
-    let headerClassBelow;
-    let headerClassNumber;
     const initialHeaderClassNumber = +Array.from(
       headers[initialIndex].classList
     ).filter(function (className) {
       return className.startsWith("H");
     })[0][1];
     let index = initialIndex;
+    let appendables = [];
 
-    if (headers[index + 1]) {
-      headerClassBelow = +Array.from(headers[index + 1].classList).filter(
-        function (className) {
-          return className.startsWith("H");
+    //Add invs icon to last element
+    headers[headers.length - 1].classList.add("invisible-icon");
+
+    if (headers[index].nextSibling) {
+      if (!headers[index].nextSibling.classList.contains("container-div")) {
+        const div = document.createElement("div");
+        div.classList.add("container-div");
+
+        while (headers[index + 1]) {
+          const headerClassBelow = +Array.from(
+            headers[index + 1].classList
+          ).filter(function (className) {
+            return className.startsWith("H");
+          })[0][1];
+          const headerClass = +Array.from(headers[index].classList).filter(
+            function (className) {
+              return className.startsWith("H");
+            }
+          )[0][1];
+
+          if (headerClassBelow > initialHeaderClassNumber) {
+            appendables.push(headers[index + 1]);
+          }
+          console.log(headerClass, headerClassBelow, index);
+          if (headerClass > headerClassBelow) {
+            headers[index].classList.add("invisible-icon");
+          }
+          if (headerClassBelow <= initialHeaderClassNumber) {
+            break;
+          }
+
+          index++;
         }
-      )[0][1];
-      headerClassNumber = initialHeaderClassNumber;
-      if (headerClassNumber < headerClassBelow) {
-        headers[index + 1].classList.toggle("hidden");
-      }
-      index++;
-    }
-    while (headers[index + 1]) {
-      headerClassBelow = +Array.from(headers[index + 1].classList).filter(
-        function (className) {
-          return className.startsWith("H");
-        }
-      )[0][1];
-      headerClassNumber = +Array.from(headers[index].classList).filter(
-        function (className) {
-          return className.startsWith("H");
-        }
-      )[0][1];
-      if (
-        headerClassNumber >= headerClassBelow &&
-        headerClassBelow > initialHeaderClassNumber
-      ) {
-        headers[index + 1].classList.toggle("hidden");
-      }
-      index++;
-      if (headerClassBelow <= initialHeaderClassNumber) {
-        break;
+
+        appendables.forEach((appendable) => {
+          div.appendChild(appendable);
+        });
+        div.classList.toggle("hidden");
+        insertAfter(headers[initialIndex], div);
+      } else {
+        headers[index].nextSibling.classList.toggle("hidden");
       }
     }
   };
 
   useEffect(() => {
-    const headers = document.querySelectorAll(".header");
-    if (headers.length > 0) {
-      let initialHeaderNumber = +Array.from(headers[0].classList).filter(
-        function (className) {
-          return className.startsWith("H");
-        }
-      )[0][1];
-      headers.forEach((header, index) => {
-        let nextHeader = headers[index + 1];
-        let nextHeaderNumber;
-        if (nextHeader) {
-          let nextClassStartingWithH = Array.from(nextHeader.classList).filter(
-            function (className) {
-              return className.startsWith("H");
-            }
-          );
-          nextHeaderNumber = +nextClassStartingWithH[0][1];
-        }
-
-        if (initialHeaderNumber >= nextHeaderNumber) {
-          initialHeaderNumber = nextHeaderNumber;
-        }
-        if (initialHeaderNumber < nextHeaderNumber && nextHeaderNumber) {
-          nextHeader.classList.add("hidden");
-        }
-      });
-    }
     const togglers = document.querySelectorAll(".section-toggler");
-
     togglers.forEach((toggler) => {
-      const hiddenHeaders = document.querySelectorAll(".hidden.header");
-      toggler.click();
-      const newHiddenHeaders = document.querySelectorAll(".hidden.header");
-
-      let arrayEq = (A, B) =>
-        A.length === B.length && A.every((e, i) => e === B[i]);
-      if (arrayEq(Array.from(hiddenHeaders), Array.from(newHiddenHeaders))) {
-        toggler.classList.add("hidden");
-      }
       toggler.click();
     });
 
-    togglers.forEach((toggler) => {});
-  }, []);
+    const invisibleIcons = document.querySelectorAll(".invisible-icon");
+    invisibleIcons.forEach((item) => {
+      item.querySelector("i").classList.add("invis");
+    });
+
+    document.querySelector("div.invis")?.classList?.remove("invis");
+    setTimeout(() => {
+      setLoading(false);
+    }, 200);
+  }, [loading]);
 
   if (sections.length === 0) {
     return <p>No sections in the article</p>;
   }
-  return sections.map((section) => {
-    return (
-      <div
-        key={section.id}
-        className={
-          "header border-start border-end border-bottom border-dark " +
-          section.nodeName +
-          " id" +
-          section.id
-        }
-      >
-        <i
-          onClick={toggleHeadersBelow}
-          className="fa-solid fa-chevron-down ps-1 pe-1 section-toggler pointer"
-        ></i>
-        <a
-          className="link-dark ps-1"
-          title={section.innerText}
-          href={"#" + section.id}
-        >
-          {section.nodeName.substring(1)}.{section.innerText.substring(0, 12)}
-          ...
-        </a>
+  return loading ? (
+    <div className="d-flex justify-content-center">
+      <div className="lds-ring">
+        <div></div>
       </div>
-    );
-  });
+    </div>
+  ) : (
+    <div className="invis">
+      {sections.map((section) => (
+        <div
+          key={section.id}
+          className={
+            "header border-start border-end border-bottom border-dark " +
+            section.nodeName +
+            " id" +
+            section.id
+          }
+        >
+          <i
+            onClick={toggleHeadersBelow}
+            className="fa-solid fa-chevron-down ps-1 pe-1 section-toggler pointer"
+          ></i>
+          <a
+            className="link-dark ps-1"
+            title={section.innerText}
+            href={"#" + section.id}
+          >
+            {section.nodeName.substring(1)}.{section.innerText.substring(0, 15)}
+            ...
+          </a>
+        </div>
+      ))}
+    </div>
+  );
 };
