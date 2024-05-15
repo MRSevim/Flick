a64db586c16a16adeb096a78bdb58e87ab6a338e
MRSevim <lionrasit@gmail.com>
2024-05-15 22:28:22 +0300

    got color modes working finally

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,7 @@
-import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap/dist/js/bootstrap.bundle";
-import "bootstrap-icons/font/bootstrap-icons.css";
-import "./index.scss";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
