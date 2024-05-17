f70cb385a4e3f09a9bb180c9f39cd441259ecfe9
MRSevim <lionrasit@gmail.com>
2024-05-17 23:31:29 +0300

    added other 2 lines that import bootstrap just incase

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,5 @@
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "./index.scss";
 import React from "react";
