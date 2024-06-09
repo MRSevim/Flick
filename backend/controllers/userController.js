0d4a06ddd009b1056fddf102c0cec86201664db3
MRSevim <lionrasit@gmail.com>
2024-06-09 18:37:07 +0300

    logout update

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -102,6 +102,8 @@ const logoutUser = (req, res) => {
   res.cookie("jwt", "", {
     httpOnly: true,
     expires: new Date(0),
+    secure: process.env.NODE_ENV !== "development", // Use secure cookies in production
+    sameSite: "none",
   });
   res.status(200).json({ message: "Logged out successfully" });
 };
