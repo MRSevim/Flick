5f976140f8f0b5a0c3ad481e6be31ed946cf4f04
MRSevim <lionrasit@gmail.com>
2024-07-01 21:59:28 +0300

    started migrating to nextjs

---
+++ nextjs-frontend/next.config.mjs
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+import dotenv from "dotenv";
+dotenv.config({ path: "../.env" });
+const nextConfig = {};
+
+export default nextConfig;
---
+++ nextjs-frontend/src/Contexts/UserContext.js
@@ -0,0 +1,29 @@
+"use client";
+import { createContext, useState, useContext } from "react";
+import ls from "localstorage-slim";
+
+const UserContext = createContext(null);
+
+const userLocalStorage = JSON.parse(ls.get("user"));
+
+const userSessionStorage = JSON.parse(sessionStorage.getItem("user"))
+  ? JSON.parse(sessionStorage.getItem("user"))
+  : undefined;
+
+export const useUserContext = () => {
+  return useContext(UserContext);
+};
+
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(
+    userLocalStorage ? userLocalStorage : userSessionStorage
+  );
+
+  return (
+    <UserContext.Provider value={[user, setUser]}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export default UserContext;
