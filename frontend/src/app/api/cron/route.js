771f4fbab02cc7e408dd078763fc13d6ccd708c4
MRSevim <lionrasit@gmail.com>
2024-08-10 18:44:22 +0300

    removed cronjobs

--- frontend/src/app/api/cron/route.js
+++
@@ -1,8 +0,0 @@
-import { envVariables } from "@/utils/HelperFuncs";
-const backendUrl = envVariables.backendUrl;
-
-export async function GET() {
-  const res = await fetch(backendUrl);
-  const data = await res.json();
-  console.log(data);
-}
